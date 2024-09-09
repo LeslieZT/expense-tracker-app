b83977cc88484a09c23981b796c1471fec727fe3
Alexa <alexamibco@gmail.com>
2024-09-09 15:17:38 -0500

    add functionality to save created expenses on local storage

---
+++ src/localstorage.js
@@ -0,0 +1,38 @@
+const category = document.getElementById("category_select");
+const amount = document.getElementById("amount");
+const date = document.getElementById("date");
+const description = document.getElementById("description");
+const submitbutton = document.querySelector(".btn-submit");
+
+class Expense {
+  constructor(category, amount, date, description) {
+    this.category = category;
+    this.amount = amount;
+    this.date = date;
+    this.description = description;
+  }
+}
+
+export const addExpense = () => {
+  let categoryValue = category.value;
+  let amountValue = amount.value;
+  let dateValue = date.value;
+  let descriptionValue = description.value;
+  let newExpense = new Expense(
+    categoryValue,
+    amountValue,
+    dateValue,
+    descriptionValue
+  );
+
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+  expenses.push(newExpense);
+
+  localStorage.setItem("expenses", JSON.stringify(expenses));
+};
+
+submitbutton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addExpense();
+  });
